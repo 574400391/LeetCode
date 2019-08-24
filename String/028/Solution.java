519f53ac12a4e393d5bfbdb78d26c450b7cd4b0a
WangHL <574400391@qq.com>
2019-08-24 14:48:38 +0800

    update

--- String/028/Solution.java
+++ String/028/Solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public int strStr(String haystack, String needle) {
             if (haystack == null || needle == null) return -1;
-
         return haystack.indexOf(needle);
     }
 }

0c5db3996406df8382c4a449beb4fa324cb80ce1
WangHL <574400391@qq.com>
2019-08-26 09:19:45 +0800

    add 088 solution

---
+++ Solution/088/Solution2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Solution2{
+
+    /**
+     * 双指针从后往前
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m - 1;
+        int q = n - 1;
+        int len = m + n - 1;
+        while ((p >= 0) && (q >= 0)) {
+            nums1[len--] = (nums1[p] > nums2[q]) ? nums1[p--] : nums2[q--];
+        }
+        System.arraycopy(nums2, 0, nums1, 0, q + 1);
+    }
+}
